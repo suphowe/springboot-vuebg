79d3ff2091bb8acbb75ffc34b3e278e863a8b31d
suphowe <suphowe@gmail.com>
2020-07-09 16:39:06 +0800

    前后端分离项目springboot后台

---
+++ src/main/java/com/soft/SpringbootVuebgApplication.java
@@ -0,0 +1,13 @@
+package com.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVuebgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVuebgApplication.class, args);
+    }
+
+}
